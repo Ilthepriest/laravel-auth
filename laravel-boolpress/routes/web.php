fec4ba27068399bee49a8b8374a5d7bdc7091e9a
Dev1 <robertodelprete1993@libero.it>
2022-06-21 18:20:04 +0200

    add exercise

--- laravel-boolpress/routes/web.php
+++ laravel-boolpress/routes/web.php
@@ -13,10 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin.')->group(function(){
+    Route::get('/', 'HomeController@index')->name('dashboard'); // /admin.dashboard
+});
+
+
+Route::get("{any?}", function (){
+    return view("guest.home");
+})->where("any", ".*");
